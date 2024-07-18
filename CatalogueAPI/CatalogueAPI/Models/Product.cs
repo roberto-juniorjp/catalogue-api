e19607c5315eb7388ceb2be50391fbe053a27996
Roberto Júnior <juniorjp@gmail.com>
2024-07-18 14:54:48 -0300

    feat: create product models

--- CatalogueAPI/CatalogueAPI/Models/Product.cs
+++ CatalogueAPI/CatalogueAPI/Models/Product.cs
@@ -1,15 +1,28 @@
-﻿namespace CatalogueAPI.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
+namespace CatalogueAPI.Models;
+
+[Table("Products")]
 public class Product
 {
+    [Key]
     public int ProductId { get; set; }
 
+    [Required(ErrorMessage="You must provide a product name!")]
+    [StringLength(80, ErrorMessage ="Product name is too big.")]
     public string? ProductName { get; set; }
 
+    [Required(ErrorMessage = "You must provide a product description!")]
+    [StringLength(300, ErrorMessage = "Product description is too big.")]
     public string? ProductDescription { get; set; }
 
+    [Required(ErrorMessage = "You must provide a product price!")]
+    [Column(TypeName = "decimal(10,2)")]
     public decimal ProductPrice { get; set; }
 
+    [Required(ErrorMessage = "You must provide a product image url!")]
+    [StringLength(300, ErrorMessage = "Product image url is too big.")]
     public string? ProductImageUrl { get; set; }
 
     public float InStock { get; set; }
