6573bf5cc159b1457e67ce41e3e12340f16f6ea4
Roberto Júnior <juniorjp@gmail.com>
2024-07-18 14:54:30 -0300

    feat: create category models

--- CatalogueAPI/CatalogueAPI/Models/Category.cs
+++ CatalogueAPI/CatalogueAPI/Models/Category.cs
@@ -1,13 +1,23 @@
-﻿namespace CatalogueAPI.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
+namespace CatalogueAPI.Models;
+
+[Table("Categories")]
 public class Category
 {
     public Category()
     => Products = [];
+    
+    [Key]
     public int CategoryId { get; set; }
-
+    
+    [Required(ErrorMessage = "You must provide a category name!")]
+    [StringLength(80, ErrorMessage = "Name is too big.")]
     public string? CategoryName { get; set; }
 
+    [Required(ErrorMessage = "You must provide a category image url!")]
+    [StringLength(300, ErrorMessage = "URL provided is too big.")]
     public string? CategoryImageUrl { get; set; }
 
     public ICollection<Product>? Products { get; set; }
